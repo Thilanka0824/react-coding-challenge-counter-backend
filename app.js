759615dbf4f8f68a78d44eeace9cbe20b324062d
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-03-30 20:31:30 -0700

    added products route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var productsRouter = require("./routes/products")
 
 var { mongoConnect } = require("./mongo.js");
 mongoConnect();
@@ -26,8 +27,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/products", productsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
